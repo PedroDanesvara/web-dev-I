115c5e6b76223fbda9fb9e81a7bfb4bbd5e6d3dd
Pedro Henrique PHD <danesvara2002@gmail.com>
2024-08-29 14:09:21 -0300

    feat: exercise 2

---
+++ js/exercises/exercises2.js
@@ -0,0 +1,22 @@
+//1. Escreva um programa que solicite um número inteiro e verifique se é divisível por 3 e por 5 ao mesmo tempo. 
+
+
+//2. Faça um programa que leia três números e informe qual é o maior e qual é o menor. 
+
+
+//3. Faça um programa que leia a idade de três pessoas e informe se alguma delas é maior de idade (idade maior ou igual a 18), se todas são maiores de idade, ou se todas são menores de idade (idade inferior a 18 anos). 
+
+
+//4. Faça um programa que solicite o nome de um dia da semana e exiba se é um dia útil (segunda a sexta-feira) ou um dia de fim de semana (sábado e domingo).
+
+
+//5. Escreva um programa que solicite ao usuário um número N e exiba todos os números primos menores que N.
+
+
+//6. Faça um programa que solicite ao usuário um número e exiba a sequência de Fibonacci até o número informado utilizando um laço de repetição.
+
+
+//7. Escreva um programa que solicite ao usuário uma lista de números, até o usuário digitar o número zero, e exiba o maior e o menor número da lista.
+
+
+//8. Escreva um programa que solicite ao usuário uma frase e exiba a quantidade de vogais na frase.
